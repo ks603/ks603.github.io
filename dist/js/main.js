7a9843868af084b7e229f0ea9599714a6d751ca9
ks603 <kevin.shaughnessy18@gmail.com>
2020-04-15 18:32:51 -0400

    Initial commit

---
+++ dist/js/main.js
@@ -0,0 +1,49 @@
+// Sticky menu background
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 150) {
+    document.querySelector('#main-nav').style.opacity = 0.9;
+  } else {
+    document.querySelector('#main-nav').style.opacity = 1;
+  }
+});
+
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 150) {
+    document.querySelector('#navbar').style.opacity = 0.9;
+  } else {
+    document.querySelector('#navbar').style.opacity = 1;
+  }
+});
+
+
+// Smooth Scrolling
+$('#main-nav a').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100
+      },
+      800
+    );
+  }
+});
+
+// Smooth Scrolling
+$('#navbar a').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100
+      },
+      800
+    );
+  }
+});
